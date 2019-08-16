98d9cf2110eaf3adc5480de81cd0e69b277f0afb
Greg Schmitt <gwizzprg100@gmail.com>
2019-08-16 12:39:53 -0400

    refactor navbar styles and removed css

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,45 +1,41 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import Slider from "rc-slider";
+import { withStyles } from "@material-ui/styles";
 import Select from "@material-ui/core/Select";
-import { MenuItem } from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
-
+import Slider from "rc-slider";
 import "rc-slider/assets/index.css";
-import "./Navbar.css";
+import styles from "./styles/NavbarStyles";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
-
     this.state = { format: "hex", open: false };
-
     this.handleFormatChange = this.handleFormatChange.bind(this);
     this.closeSnackbar = this.closeSnackbar.bind(this);
   }
-
   handleFormatChange(e) {
     this.setState({ format: e.target.value, open: true });
     this.props.handleChange(e.target.value);
   }
-
   closeSnackbar() {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel, showingAllColors } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className="Navbar">
-        <div className="logo">
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
           <Link to="/">reactcolorpicker</Link>
         </div>
         {showingAllColors && (
-          <div className="slider-container">
+          <div>
             <span>Level: {level}</span>
-            <div className="slider">
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -50,17 +46,14 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className="select-container">
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - #ffffff</MenuItem>
-            <MenuItem value="rgb">rgb - rgb(255, 255, 255, 255)</MenuItem>
-            <MenuItem value="rgba">
-              RGBA - rgb(255, 255, 255, 255, 1.0)
-            </MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
         <Snackbar
-          onClose={this.closeSnackbar}
           anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
           open={this.state.open}
           autoHideDuration={3000}
@@ -69,9 +62,10 @@ class Navbar extends Component {
               Format Changed To {format.toUpperCase()}
             </span>
           }
-          contentProps={{
+          ContentProps={{
             "aria-describedby": "message-id"
           }}
+          onClose={this.closeSnackbar}
           action={[
             <IconButton
               onClick={this.closeSnackbar}
@@ -87,5 +81,4 @@ class Navbar extends Component {
     );
   }
 }
-
-export default Navbar;
+export default withStyles(styles)(Navbar);
